2cb80a804dc3c1f1c1ea4f4d8188ad5cb938e7f4
ntbao10122003 <nguyenthebao1711@gmail.com>
2025-03-29 13:52:13 +0700

    fetch-resget

--- src/view/frontend/pages/MemberCenter.js
+++ src/view/frontend/pages/MemberCenter.js
@@ -16,21 +16,21 @@ const MemberCenter = () => {
                <div className='list-banence-member'>
                    <div className='item-member-bannence'>
                       <Link to="/LayoutProfileMobile/Deposit">
-                       <div className='icon-banecer'></div>
+                       <div className='icon-banecer' style={{ backgroundImage: 'url("/deposit.82a67952a69a57c9.png")' }} ></div>
                        <p>Nạp Tiền</p>
                       </Link>
                    </div>
 
                    <div className='item-member-bannence'>
                       <Link to="/LayoutProfileMobile/withdrawApplication">
-                       <div className='icon-banecer'></div>
+                       <div className='icon-banecer' style={{ backgroundImage: 'url("/withdraw.5b6bb9a91338cfc8.png")' }}></div>
                        <p>Rút Tiền</p>
                       </Link>
                    </div>
 
                    <div className='item-member-bannence'>
-                      <Link to="/LayoutProfileMobile/wellet">
-                       <div className='icon-banecer'></div>
+                      <Link to="/LayoutProfileMobile/wellet"  >
+                       <div className='icon-banecer' style={{ backgroundImage: 'url("/any-time-discount.c0680e5396ff9140.png")' }}></div>
                        <p>Ví</p>
                       </Link>
                    </div>
@@ -44,21 +44,21 @@ const MemberCenter = () => {
 
                    <div className='item-member-bannence'>
                       <Link to="/LayoutProfileMobile/history">
-                       <div className='icon-banecer'></div>
+                       <div className='icon-banecer' style={{ backgroundImage: 'url("/bet-record.30a422553e182818.png")' }} ></div>
                        <p>Lịch sử cược</p>
                       </Link>
                    </div>
 
                    <div className='item-member-bannence'>
                       <Link to="/LayoutProfileMobile/changeBankAccount">
-                       <div className='icon-banecer'></div>
+                       <div className='icon-banecer ' style={{ backgroundImage: 'url("/withdraw.5b6bb9a91338cfc8.png")' }}></div>
                        <p>Cài đặt rút tiền</p>
                       </Link>
                    </div>
 
                    <div className='item-member-bannence'>
                       <Link to="/LayoutProfileMobile/historyRut">
-                       <div className='icon-banecer'></div>
+                       <div className='icon-banecer' style={{ backgroundImage: 'url("/vip.ec8c7334af241e8f.png")' }} ></div>
                        <p>Lịch sử nạp rút</p>
                       </Link>
                    </div>
@@ -66,7 +66,7 @@ const MemberCenter = () => {
 
                    <div className='item-member-bannence'>
                       <Link to="/">
-                       <div className='icon-banecer'></div>
+                       <div className='icon-banecer' style={{ backgroundImage: 'url("/announcement.db4b198579b876dc.png")' }} ></div>
                        <p>Đăng xuất</p>
                       </Link>
                    </div>
